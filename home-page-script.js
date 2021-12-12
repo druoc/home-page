7189a0d1d05e288c6c0839aeecc5627350d7e6f3
Dave <d.ruocco1987@googlemail.com>
2021-12-12 15:03:41 +0000

    Added CoinGecko api fetch function

--- home-page-script.js
+++ home-page-script.js
@@ -1,5 +1,5 @@
 //using the Fetch api to return a random picture from the Unplash api
-//which we then render as a background image and author to the page
+//which we then render on each load/refresh as a background image, and author of the picture to the 'author' p-tag.
 
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=space")
     .then(res => res.json())
@@ -8,3 +8,10 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.querySelector('.author').textContent = `By: ${data.user.name}`
     });
     
+//again using the fetch api to grab the price of bitcoin from the CoinGeck api
+fetch('https://api.coingecko.com/api/v3/coins/bitcoin')
+    .then(res => res.json())
+    .then( data => {
+        document.querySelector('.crypto').textContent = `Bitcoin: £${data.market_data.current_price.gbp}`
+    })
+    .catch(error => console.log('Whoops, something went wrong...'))
