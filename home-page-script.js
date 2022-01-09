9b429c60f9a9f2e2ebb93e9c468e3a2c81c5a78d
Dave <d.ruocco1987@googlemail.com>
2022-01-09 15:17:15 +0000

    Added current time display

--- home-page-script.js
+++ home-page-script.js
@@ -5,13 +5,25 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     .then(res => res.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.regular})`
-        document.querySelector('.author').textContent = `By: ${data.user.name}`
+        document.querySelector('.author').textContent = `Photo by: ${data.user.name}`
     });
     
-//again using the fetch api to grab the price of bitcoin from the CoinGeck api
+//again using the fetch api to grab the price of bitcoin from the CoinGecko api
 fetch('https://api.coingecko.com/api/v3/coins/bitcoin')
     .then(res => res.json())
     .then( data => {
         document.querySelector('.crypto').textContent = `Bitcoin: £${data.market_data.current_price.gbp}`
+        document.querySelector('.crypto-icon').innerHTML = `
+        <img src="${data.image.thumb}"/>`
     })
     .catch(error => console.log('Whoops, something went wrong...'))
+
+//grabbing the current time, and rendering it to the page
+function renderTime() {
+    const today = new Date();
+    document.querySelector('.time').textContent = today.toLocaleTimeString("en-uk", {timeStyle: "short"});
+}
+
+setInterval(renderTime, 1000);
+
+
