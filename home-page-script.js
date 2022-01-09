0a2a5bebf427bba98695593aa2a4fa40f3561470
Dave <d.ruocco1987@googlemail.com>
2022-01-09 17:40:34 +0000

    Added CSS text fade in animation

--- home-page-script.js
+++ home-page-script.js
@@ -36,7 +36,6 @@ navigator.geolocation.getCurrentPosition(position => {
             return res.json()
         })
         .then(data => {
-            console.log(data)
             const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
             document.querySelector('.weather').innerHTML = `
             <img src=${iconUrl} />
